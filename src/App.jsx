f8340618795b18fc13fcf874fd7d7f41476fe7a0
dineshsrisai <dineshsrisai1@gmail.com>
2026-03-10 23:47:51 +0530

    Created Routes and Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import NavBar from "./NavBar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <>
-      <NavBar/>
+      <BrowserRouter baseName="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
-
 export default App;
