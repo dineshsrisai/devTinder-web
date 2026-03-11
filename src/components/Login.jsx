eefb5492417ec9fd3a5611a5990ed3336e3199cb
dineshsrisai <dineshsrisai1@gmail.com>
2026-03-11 23:02:11 +0530

    changed theme

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,17 @@
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="flex justify-center items-center my-10">
+      <div className="card bg-base-300 w-96 shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title">Login</h2>
+          <div></div>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Login</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
+
 export default Login;
