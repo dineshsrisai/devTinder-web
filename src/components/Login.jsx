096ca6655a751277bcc17bb5afb7b218919af483
dineshsrisai <dineshsrisai1@gmail.com>
2026-03-12 23:36:51 +0530

    added login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,44 @@
+import { useState } from "react";
+
 const Login = () => {
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className="flex justify-center items-center my-10">
       <div className="card bg-base-300 w-96 shadow-sm">
         <div className="card-body">
-          <h2 className="card-title">Login</h2>
-          <div></div>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Login</button>
+          <h2 className="card-title justify-center">Login</h2>
+          <div className="form-control">
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Email ID</legend>
+              <input
+                type="email"
+                className="input input-info rounded-lg"
+                placeholder="email"
+                value={emailId}
+                onChange={(e) => {
+                  setEmailId(e.target.value);
+                }}
+              />
+            </fieldset>
+          </div>
+          <div className="form-control">
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Password</legend>
+              <input
+                type="password"
+                className="input input-info rounded-lg"
+                placeholder="password"
+                value={password}
+                onChange={(e)=>{
+                  setPassword(e.target.value);
+                }}
+              />
+            </fieldset>
+          </div>
+          <div className="card-actions justify-center pt-3">
+            <button className="btn btn-info rounded-lg">Login</button>
           </div>
         </div>
       </div>
