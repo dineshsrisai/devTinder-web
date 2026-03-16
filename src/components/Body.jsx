76eac217d53d45c738dbcdf0db65d5fdf6fca712
dineshsrisai <dineshsrisai1@gmail.com>
2026-03-16 23:26:06 +0530

    added links

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,22 +3,24 @@ import NavBar from "./NavBar";
 import Footer from "./Footer";
 import { BASE_URL } from "../utils/constants";
 import axios from "axios";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { addUser } from "../utils/userSlice";
 
 const Body = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  
+  const userData = useSelector((store) => store.user);
+
   const fetchUser = async () => {
+    if (!userData) return;
     try {
       const res = await axios.get(BASE_URL + "/profile/view", {
         withCredentials: true,
       });
       dispatch(addUser(res.data));
     } catch (e) {
-      if(e.status === 401){
+      if (e.status === 401) {
         navigate("/login");
       }
     }
