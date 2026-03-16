3609bd4c0f71bf785529c314cb1197b0131f90c7
dineshsrisai <dineshsrisai1@gmail.com>
2026-03-16 23:12:55 +0530

    redirecting to login if !loggedIn

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,12 +1,38 @@
-import {Outlet} from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
+import { BASE_URL } from "../utils/constants";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { addUser } from "../utils/userSlice";
+
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  
+  const fetchUser = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      dispatch(addUser(res.data));
+    } catch (e) {
+      if(e.status === 401){
+        navigate("/login");
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <div>
       <NavBar />
-      <Outlet/>
-      <Footer/>
+      <Outlet />
+      <Footer />
     </div>
   );
 };
