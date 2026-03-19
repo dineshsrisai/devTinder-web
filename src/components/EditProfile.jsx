fccdc9084257635d2d13c38d11e0f991f7bc6ea6
dineshsrisai <dineshsrisai1@gmail.com>
2026-03-19 23:59:04 +0530

    Finally fixed the css

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -1,94 +1,84 @@
 import { useState } from "react";
+import UserCard from "../components/UserCard";
 
-const EditProfile = ({user}) => {
+const EditProfile = ({ user }) => {
   const [firstName, setFirstName] = useState(user.firstName);
   const [lastName, setLastName] = useState(user.lastName);
   const [photoUrl, setPhotoUrl] = useState(user.photoUrl);
-
   const [age, setAge] = useState(user.age);
   const [about, setAbout] = useState(user.about);
 
-  const [error, setError] = useState("");
-
   return (
-    <div className="flex justify-center items-center py-10">
-      <div className="card bg-base-300 w-96 shadow-sm">
-        <div className="card-body py-5">
-          <h2 className="card-title justify-center">Edit Profile</h2>
-          <div className="form-control">
-            <fieldset className="fieldset">
-              <legend className="fieldset-legend">FirstName</legend>
-              <input
-                type="text"
-                className="input input-info rounded-lg"
-                placeholder=""
-                value={firstName}
-                onChange={(e) => {
-                  setFirstName(e.target.value);
-                }}
-              />
-            </fieldset>
-          </div>
-          <div className="form-control">
-            <fieldset className="fieldset">
-              <legend className="fieldset-legend">LastName</legend>
-              <input
-                type="text"
-                className="input input-info rounded-lg"
-                placeholder=""
-                value={lastName}
-                onChange={(e) => {
-                  setLastName(e.target.value);
-                }}
-              />
-            </fieldset>
-            <fieldset className="fieldset">
-              <legend className="fieldset-legend">PhotoUrl</legend>
-              <input
-                type="text"
-                className="input input-info rounded-lg"
-                placeholder=""
-                value={photoUrl}
-                onChange={(e) => {
-                  setPhotoUrl(e.target.value);
-                }}
-              />
-            </fieldset>
-            <div className="form-control"></div>
-          </div>
-          <div className="form-control">
-            <fieldset className="fieldset">
-              <legend className="fieldset-legend">Age</legend>
-              <input
-                type="number"
-                className="input input-info rounded-lg"
-                placeholder=""
-                value={age}
-                onChange={(e) => {
-                  setAge(e.target.value);
-                }}
-              />
-            </fieldset>
-          </div>
-          <div className="form-control">
-            <fieldset className="fieldset">
-              <legend className="fieldset-legend">About</legend>
-              <input
-                type="text"
-                className="input input-info rounded-lg"
-                placeholder=""
-                value={about}
-                onChange={(e) => {
-                  setAbout(e.target.value);
-                }}
-              />
-            </fieldset>
-          </div>
-          <p className="text-red-500">{error}</p>
-          <div className="card-actions justify-center">
-            <button className="btn btn-info rounded-lg">Save Changes</button>
+    <div className="flex justify-center items-stretch gap-10 my-10">
+      <div className="py-4">
+        <div className="card bg-base-300 w-96 shadow-sm h-full flex flex-col">
+          <div className="card-body py-5 flex-grow">
+            <h2 className="card-title justify-center">Edit Profile</h2>
+            <div className="form-control">
+              <fieldset className="fieldset">
+                <legend className="fieldset-legend">FirstName</legend>
+                <input
+                  type="text"
+                  className="input input-info rounded-lg"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+              </fieldset>
+            </div>
+            <div className="form-control">
+              <fieldset className="fieldset">
+                <legend className="fieldset-legend">LastName</legend>
+                <input
+                  type="text"
+                  className="input input-info rounded-lg"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+              </fieldset>
+              <fieldset className="fieldset">
+                <legend className="fieldset-legend">PhotoUrl</legend>
+                <input
+                  type="text"
+                  className="input input-info rounded-lg"
+                  value={photoUrl}
+                  onChange={(e) => setPhotoUrl(e.target.value)}
+                />
+              </fieldset>
+            </div>
+            <div className="form-control">
+              <fieldset className="fieldset">
+                <legend className="fieldset-legend">Age</legend>
+                <input
+                  type="number"
+                  className="input input-info rounded-lg"
+                  value={age}
+                  onChange={(e) => setAge(e.target.value)}
+                />
+              </fieldset>
+            </div>
+            <div className="form-control">
+              <fieldset className="fieldset">
+                <legend className="fieldset-legend">About</legend>
+                <input
+                  type="text"
+                  className="input input-info rounded-lg"
+                  value={about}
+                  onChange={(e) => setAbout(e.target.value)}
+                />
+              </fieldset>
+            </div>
+            <div className="card-actions justify-center mt-auto pt-4">
+              <button className="btn btn-info rounded-lg">Save Changes</button>
+            </div>
           </div>
         </div>
+      </div>
+      
+      <div className="py-4 w-96">
+        <UserCard 
+          user={{ firstName, lastName, photoUrl, age, about }} 
+          className="h-full w-full" 
+        />
       </div>
     </div>
   );
