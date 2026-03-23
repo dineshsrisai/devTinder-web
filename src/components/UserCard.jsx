69ca4000f77765a97d293040aeb7cd380f359877
dineshsrisai <dineshsrisai1@gmail.com>
2026-03-23 23:34:33 +0530

    building the feed

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,8 +1,26 @@
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { removeFeed } from "../utils/feedSlice";
+
 const UserCard = ({ user }) => {
+  const dispatch = useDispatch();
+  if (!user) return null;
 
-  if(!user) return null;
+  const { _id, firstName, lastName, photoUrl, age, gender, about } = user;
 
-  const { firstName, lastName, photoUrl, age, gender, about } = user;
+  const handleSendRequest = async (status, userId) => {
+    try {
+      await axios.post(
+        BASE_URL + "/request/send" + "/" + status + "/" + userId,
+        {},
+        { withCredentials: true },
+      );
+      dispatch(removeFeed(userId));
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   return (
     <div className="card bg-base-300 w-96 shadow-sm">
@@ -14,8 +32,18 @@ const UserCard = ({ user }) => {
         {age && gender && <p>{age + ", " + gender}</p>}
         <p>{about}</p>
         <div className="card-actions justify-center my-4">
-          <button className="btn btn-soft btn-error">Ignore</button>
-          <button className="btn btn-soft btn-info">Interested</button>
+          <button
+            className="btn btn-soft btn-error"
+            onClick={() => handleSendRequest("ignored", _id)}
+          >
+            Ignore
+          </button>
+          <button
+            className="btn btn-soft btn-info"
+            onClick={() => handleSendRequest("interested", _id)}
+          >
+            Interested
+          </button>
         </div>
       </div>
     </div>
